8167063049179c37dc8b72ba83f70a4caadb4384
itay <itay@90min.com>
2016-07-25 17:02:10 +0300

    Added token validation from FB

--- hello.rb
+++ hello.rb
@@ -1,5 +1,17 @@
 require 'sinatra'
 
+VALIDATION_TOKEN = "dc3aab2a-3c08-4d76-a590-7e61a7a7a80a".freeze
+
 get '/' do
   "Hello World!"
+end
+
+get '/callback' do
+  if params['hub.mode'] == 'subscriber' &&
+     params['hub.verify_token'] == VALIDATION_TOKEN
+     
+    params['hub.challenge']
+  else
+    403
+  end  
 end
