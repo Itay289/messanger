9d6b3081ef826f262df6b6ad6bf7c10a6f985d7e
itay <itay@90min.com>
2016-07-25 21:13:39 +0300

    handle request

--- app.rb
+++ app.rb
@@ -1,7 +1,10 @@
 require 'sinatra'
 require 'json'
+require 'uri'
+require 'net/http'
 
 VALIDATION_TOKEN = "dc3aab2a-3c08-4d76-a590-7e61a7a7a80a".freeze
+PAGE_ACCESS_TOKEN = 'EAADpxMcwG4wBABKQfiXDApHTaQUI78iFu3cZBPZCjVlqnAR0so6RHNq1R8G0c6YTFgQZBbs8uAl1waKZAbsyGOPm8tbn2uZAZAecUXZAApeif305TcFMoaq5iTdkgqkl2R1GQVf2Jcfe3Vp64unZBOnJZA9VQZAZBZAmPaY4ZADyF2su2XAZDZD'.freeze
 
 get '/' do
   "Hello World!"
@@ -25,10 +28,32 @@ post '/callback' do
     msg
     sender = msg["sender"]["id"]
     if msg["message"] && msg["message"]["text"]
-      payload = msg["postback"]["payload"]
-      puts "Sender ID: #{sender}, Text: #{payload}"
+      handel_requst(msg["message"]["text"], sender)
     end
   end
+end
+
+def handel_requst(text, sender)
+  case text
+  when 'hi' || 'Hi'
+    send_message('hi', sender)
+  else
+    send_message("i don't understand", sender)
+  end
+end
+
+def send_message(message, sender)
+  uri = URI('https://graph.facebook.com/v2.6/me/messages')
+  https = Net::HTTP.new(uri.host, uri.port)
+  https.use_ssl = true
+
+  request = Net::HTTP::Post.new(uri.path)
+
+  request["access_token"] = PAGE_ACCESS_TOKEN
+  request["HEADER2"] = 'VALUE2'
+
+  response = https.request(request)
+  response
 end
 
 # def payload(sender, payload)
