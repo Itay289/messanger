3e7b472f25646bc1d59255539710488555a8dfdb
itay <itay@90min.com>
2016-07-26 11:37:39 +0300

    case statement

--- app.rb
+++ app.rb
@@ -54,63 +54,59 @@ def text_message_request_body(sender, text)
     recipient: {
       id: sender
     },
-    response(text)
+    message: {
+      response(text)
+    }  
   }.to_json  
 end
 
 def response(text)
   case text
   when 'hi'
-    message: {
-      text: 'Hi, what team do you want to follow'
-    }  
+    text: 'Hi, what team do you want to follow'
   when "What's up?"
-    message: {
-      text: 'Very good thank you, what team do you want to follow'
-    }  
+    text: 'Very good thank you, what team do you want to follow'
   else
     generic_message
   end      
 end
 
 def generic_message
-  message: {
-    attachment: {
-      type: "template",
-      payload: {
-        template_type: "generic",
-        elements: [{
-          title: "rift",
-          subtitle: "Next-generation virtual reality",
-          item_url: "https://www.oculus.com/en-us/rift/",               
-          image_url: "http://messengerdemo.parseapp.com/img/rift.png",
-          buttons: [{
-            type: "web_url",
-            url: "https://www.oculus.com/en-us/rift/",
-            title: "Open Web URL"
-          }, {
-            type: "postback",
-            title: "Call Postback",
-            payload: "Payload for first bubble",
-          }],
+  attachment: {
+    type: "template",
+    payload: {
+      template_type: "generic",
+      elements: [{
+        title: "rift",
+        subtitle: "Next-generation virtual reality",
+        item_url: "https://www.oculus.com/en-us/rift/",               
+        image_url: "http://messengerdemo.parseapp.com/img/rift.png",
+        buttons: [{
+          type: "web_url",
+          url: "https://www.oculus.com/en-us/rift/",
+          title: "Open Web URL"
         }, {
-          title: "touch",
-          subtitle: "Your Hands, Now in VR",
-          item_url: "https://www.oculus.com/en-us/touch/",               
-          image_url: "http://messengerdemo.parseapp.com/img/touch.png",
-          buttons: [{
-            type: "web_url",
-            url: "https://www.oculus.com/en-us/touch/",
-            title: "Open Web URL"
-          }, {
-            type: "postback",
-            title: "Call Postback",
-            payload: "Payload for second bubble",
-          }]
+          type: "postback",
+          title: "Call Postback",
+          payload: "Payload for first bubble",
+        }],
+      }, {
+        title: "touch",
+        subtitle: "Your Hands, Now in VR",
+        item_url: "https://www.oculus.com/en-us/touch/",               
+        image_url: "http://messengerdemo.parseapp.com/img/touch.png",
+        buttons: [{
+          type: "web_url",
+          url: "https://www.oculus.com/en-us/touch/",
+          title: "Open Web URL"
+        }, {
+          type: "postback",
+          title: "Call Postback",
+          payload: "Payload for second bubble",
         }]
-      }
+      }]
     }
-  }  
+  }
 end
 
 
