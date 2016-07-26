6d1515d3b4952cffbdd2ea6b63cc21b8bce03cf4
itay <itay@90min.com>
2016-07-26 11:28:38 +0300

    case statement

--- app.rb
+++ app.rb
@@ -50,58 +50,66 @@ def bot_response(sender, text)
 end
 
 def text_message_request_body(sender, text)
-  # {
-  #   recipient: {
-  #     id: sender
-  #   },
-  #   message: {
-  #     text: text
-  #   }
-  # }.to_json
   {
     recipient: {
       id: sender
     },
-    message: {
-      attachment: {
-        type: "template",
-        payload: {
-          template_type: "generic",
-          elements: [{
-            title: "rift",
-            subtitle: "Next-generation virtual reality",
-            item_url: "https://www.oculus.com/en-us/rift/",               
-            image_url: "http://messengerdemo.parseapp.com/img/rift.png",
-            buttons: [{
-              type: "web_url",
-              url: "https://www.oculus.com/en-us/rift/",
-              title: "Open Web URL"
-            }, {
-              type: "postback",
-              title: "Call Postback",
-              payload: "Payload for first bubble",
-            }],
-          }, {
-            title: "touch",
-            subtitle: "Your Hands, Now in VR",
-            item_url: "https://www.oculus.com/en-us/touch/",               
-            image_url: "http://messengerdemo.parseapp.com/img/touch.png",
-            buttons: [{
-              type: "web_url",
-              url: "https://www.oculus.com/en-us/touch/",
-              title: "Open Web URL"
-            }, {
-              type: "postback",
-              title: "Call Postback",
-              payload: "Payload for second bubble",
-            }]
-          }]
-        }
-      }
-    }
+    response(text)
   }.to_json  
 end
 
 def response(text)
-  text
+  case text
+  when 'hi' || 'Hi'
+    message: 'Hi,' 
+    'Nice to meet you,'
+    'what team do you want to follow'
+  when "What's up?" || "whats up"
+    'Very good thank you,'
+    'what team do you want to follow'
+  else
+    generic_message
+  end      
 end
+
+def generic_message
+  message: {
+    attachment: {
+      type: "template",
+      payload: {
+        template_type: "generic",
+        elements: [{
+          title: "rift",
+          subtitle: "Next-generation virtual reality",
+          item_url: "https://www.oculus.com/en-us/rift/",               
+          image_url: "http://messengerdemo.parseapp.com/img/rift.png",
+          buttons: [{
+            type: "web_url",
+            url: "https://www.oculus.com/en-us/rift/",
+            title: "Open Web URL"
+          }, {
+            type: "postback",
+            title: "Call Postback",
+            payload: "Payload for first bubble",
+          }],
+        }, {
+          title: "touch",
+          subtitle: "Your Hands, Now in VR",
+          item_url: "https://www.oculus.com/en-us/touch/",               
+          image_url: "http://messengerdemo.parseapp.com/img/touch.png",
+          buttons: [{
+            type: "web_url",
+            url: "https://www.oculus.com/en-us/touch/",
+            title: "Open Web URL"
+          }, {
+            type: "postback",
+            title: "Call Postback",
+            payload: "Payload for second bubble",
+          }]
+        }]
+      }
+    }
+  }  
+end
+
+
